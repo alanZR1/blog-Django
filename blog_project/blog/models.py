97da44173fcdde5fee7f3b1636ff551d8cf66fbe
Alan Reyes Luis <alannreyes02@gmail.com>
2025-05-21 11:43:09 -0600

    creacion de los modelos Post y Comment

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField() 
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # cascade es una opción de eliminación en cascada de elementos relacionados
+    created_at = models.DateTimeField(auto_now_add=True) 
+    
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.foreignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
