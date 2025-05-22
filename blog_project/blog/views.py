c2b38a3453f1be482b542f5e061782c4a3b9f96c
Alan Reyes Luis <alannreyes02@gmail.com>
2025-05-22 16:25:16 -0600

    creando las vistas para post y listar post

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+def lista_posts(request):
+    post = Post.objects.all().order_by('-creado')
+    return render(request, 'blog/lista_posts.html', {'posts': post})
+
+
+@login_required
+def crear_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.autor = request.user
+            new_post.save()
+            return redirect('lista_posts')
+    else:
+        form = PostForm()
+    return render(request, 'blog/crear_post.html', {'form': form})
